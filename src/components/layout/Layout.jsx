04f1a2c3a57dbd5a60d377fa9da4882ac7ae6f20
Reza Tavakkoli <immohammadrezatavakkoli@gmail.com>
2024-02-01 01:04:09 +0330

    Create Provider.js file in redux toolkit

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,17 +1,16 @@
 import Navbar from "@/module/Navbar";
-import store from "@/redux_toolkit/store";
-import { Provider } from "react-redux";
+import Providers from "@/redux_toolkit/Providers";
 
 const Layout = ({ children }) => {
   return (
-    <Provider store={store}>
+    <Providers>
       <div className="max-lg:hidden max-md:hidden max-sm:hidden">
         <header className="w-full h-24 absolute">
           <Navbar />
         </header>
         <main>{children}</main>
       </div>
-    </Provider>
+    </Providers>
   );
 };
 
